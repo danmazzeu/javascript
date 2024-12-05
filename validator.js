015f0f053008aede122a181b234c75267ae43359
danmazzeu <danmazzeu9@gmail.com>
2024-12-05 07:37:44 -0300

    Update validator.js

--- validator.js
+++ validator.js
@@ -1,85 +1,85 @@
 export class Validator {
     constructor() {
-        this.regexNomeCompleto = /^[A-Z][a-z]+\s[A-Z][a-z]+$/;
-        this.regexNumeros = /^-?\d+$/;
-        this.regexDecimais = /^-?\d+(\.\d+)?$/;
-        this.regexCPF = /^[0-9]{11}$/;
-        this.regexCNPJ = /^[0-9]{14}$/;
-        this.regexRG = /^\d{9,12}$/;
-        this.regexSenha = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-        this.regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        this.regexData = /^\d{2}\/\d{2}\/\d{4}$/;
-        this.regexHora = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
-        this.regexVazio = /^\s*\S+\s*$/;
-    }
-  
-    validarNomeCompleto(nomeCompleto) {
-        return this.regexNomeCompleto.test(nomeCompleto);
+        this.fullNameRegex = /^[A-Z][a-z]+\s[A-Z][a-z]+$/;
+        this.numberRegex = /^-?\d+$/;
+        this.decimalRegex = /^-?\d+(\.\d+)?$/;
+        this.cpfRegex = /^[0-9]{11}$/;
+        this.cnpjRegex = /^[0-9]{14}$/;
+        this.rgRegex = /^\d{9,12}$/;
+        this.passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        this.emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        this.dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+        this.timeRegex = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+        this.emptyRegex = /^\s*\S+\s*$/;
     }
 
-    validarNumero(numero) {
-        numero = numero.replace(/[^\dA-Z]/g, '');
-        return this.regexNumeros.test(numero);
+    validateFullName(fullName) {
+        return this.fullNameRegex.test(fullName);
     }
 
-    validarVazio(decimais) {
-        return this.regexDecimais.test(decimais);
+    validateNumber(number) {
+        number = number.replace(/[^\dA-Z]/g, '');
+        return this.numberRegex.test(number);
     }
 
-    validarCPF(cpf) {
+    validateDecimal(decimal) {
+        return this.decimalRegex.test(decimal);
+    }
+
+    validateCPF(cpf) {
         cpf = cpf.replace(/[^\dA-Z]/g, '');
-        return this.regexCPF.test(cpf);
+        return this.cpfRegex.test(cpf);
     }
 
-    validarCPF(cnpj) {
+    validateCNPJ(cnpj) {
         cnpj = cnpj.replace(/[^\dA-Z]/g, '');
-        return this.regexCNPJ.test(cnpj);
+        return this.cnpjRegex.test(cnpj);
     }
-  
-    validarRG(rg) {
+
+    validateRG(rg) {
         rg = rg.replace(/[^\dA-Z]/g, '');
-        return this.regexRG.test(rg);
+        return this.rgRegex.test(rg);
     }
 
-    validarSenha(senha) {
-        return this.regexSenha.test(senha);
-    }
-  
-    validarEmail(email) {
-        return this.regexEmail.test(email);
-    }
-  
-    validarData(data) {
-        return this.regexData.test(data);
-    }
-  
-    validarHora(hora) {
-        return this.regexHora.test(hora);
+    validatePassword(password) {
+        return this.passwordRegex.test(password);
     }
 
-    validarVazio(valor) {
-        return this.regexVazio.test(valor);
+    validateEmail(email) {
+        return this.emailRegex.test(email);
+    }
+
+    validateDate(date) {
+        return this.dateRegex.test(date);
+    }
+
+    validateTime(time) {
+        return this.timeRegex.test(time);
+    }
+
+    validateNotEmpty(value) {
+        return this.emptyRegex.test(value);
     }
 }
 
 /* HTML
-    <script defer src="validator.js" type="module"></script>
+<script defer src="validator.js" type="module"></script>
 */
 
-/*  JAVASCRIPT
-    import { Validator } from './validator.js';
+/* JAVASCRIPT
+import { Validator } from './validator.js';
 
-    const field = document.getElementById('field');
-    const btn = document.getElementById('btn');
+const field = document.getElementById('field');
+const btn = document.getElementById('btn');
 
-    btn.addEventListener('click', () => {
-        const value = field.value;
-        const validator = new Validator();
+btn.addEventListener('click', () => {
+    const value = field.value;
+    const validator = new Validator();
 
-        if (validator.validarNumero(value)) {
-            alert('ok');
-        } else {
-            alert('error');
-        }
-    });
+    if (validator.validateNumber(value)) {
+        alert('ok');
+    } else {
+        alert('error');
+    }
+});
 */
