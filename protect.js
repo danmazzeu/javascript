e3a63fbd5f2cc3245830f6087ded5e1c1f30bbad
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-01-11 15:52:11 -0300

    Update protect.js

--- protect.js
+++ protect.js
@@ -1,3 +1,8 @@
 document.addEventListener('contextmenu', (event) => {
     event.preventDefault();
 });
+
+const dprotect = new Date();
+
+console.log("Elaborado e Desenvolvido por Daniel Mazzeu");
+console.log("Todos os direitos reservados " + dprotect.getFullYear() + ".");
